66f63f86bc0d868508ed1fb585d65a990bfdba44
SweeTeaRex <Trey.m.adams92@gmail.com>
2024-07-15 04:28:14 -0500

    You know it

--- Pi.c
+++ Pi.c
@@ -9,10 +9,16 @@
 
 int main(void)
 {
+    int place;
 
+    printf("How many places?\n");
+    scanf("%i", &place);
+
+    double pi = 3.14159265358979323846;
     
-
-
+    
+    printf("Pi is %.*lf \n", place, pi);
+    
     return 0;
 
 
